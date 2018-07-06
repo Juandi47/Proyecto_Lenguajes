21a74f46bd1f63ea7bfcc342b0b4bbdb56cc67f0
juandi perez <juandi199847@hotmail.com>
2018-07-06 01:32:36 -0600

    Calses de orden

---
+++ PedidosExpress/BL/BLOrden.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class BLOrden
+    {
+        public string Cedula { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido1 { get; set; }
+
+        public string Apellido2 { get; set; }
+
+        public int Codigo_Orden { get; set; }
+
+        public string Codigo_Plato { get; set; }
+
+        public string Nombre_Plato { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public BLOrden(string cedula, string nombre, string apellido1, string apellido2, int codigo_Orden, string codigo_Plato, string nombre_Plato, int cantidad)
+        {
+            Cedula = cedula;
+            Nombre = nombre;
+            Apellido1 = apellido1;
+            Apellido2 = apellido2;
+            Codigo_Orden = codigo_Orden;
+            Codigo_Plato = codigo_Plato;
+            Nombre_Plato = nombre_Plato;
+            Cantidad = cantidad;
+        }
+
+        public BLOrden() { }  
+
+    }
+}
