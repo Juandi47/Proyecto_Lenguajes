7e3f91d0753e20c89fcb8cfd34970e3b99ca1cc1
juandi perez <juandi199847@hotmail.com>
2018-07-06 06:09:45 -0600

    Hice el metodo de llenar la informacion de las ordenes

--- PedidosExpress/UI/CocinaListaPedidosWF.aspx.cs
+++ PedidosExpress/UI/CocinaListaPedidosWF.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BL;
 
 namespace UI
 {
@@ -12,6 +13,80 @@ namespace UI
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            LlenarGrids();
+            
+
         }
+
+        protected void LlenarGrids() {
+
+            ManejadorOrden orden = new ManejadorOrden();
+
+            List<BLOrden> lista = new List<BLOrden>();
+
+            lista = orden.DevolverPedidos();
+            if (lista.Count > 0)
+            {
+
+                grdInfoOrden1.DataSource = lista;
+
+                grdInfoOrden1.DataBind();
+
+                orden.CantidadGridsLlenos += 1;
+
+                lista = orden.DevolverPedidos();
+
+                if (lista.Count > 0)
+                {
+
+                    grdInfoOrden2.DataSource = lista;
+
+                    grdInfoOrden2.DataBind();
+
+                    orden.CantidadGridsLlenos += 1;
+
+                    lista = orden.DevolverPedidos();
+                    if (lista.Count > 0)
+                    {
+
+                        grdInfoOrden3.DataSource = lista;
+
+                        grdInfoOrden3.DataBind();
+
+                        orden.CantidadGridsLlenos += 1;
+
+                        lista = orden.DevolverPedidos();
+                        if (lista.Count > 0)
+                        {
+
+                            grdInfoOrden4.DataSource = lista;
+
+                            grdInfoOrden4.DataBind();
+
+                            lista = orden.DevolverPedidos();
+                            if (lista.Count > 0)
+                            {
+
+                                grdInfoOrden5.DataSource = lista;
+
+                                grdInfoOrden5.DataBind();
+
+                                orden.CantidadGridsLlenos += 1;
+
+                            }
+                        }
+                    }
+
+                }
+
+            }      
+
+         }
+
+        protected void entregarBTN1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
     }
 }
