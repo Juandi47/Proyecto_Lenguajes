9015567a8d22a1047d6f880d7f438ca41ebe326a
Anthony1210 <anthony.castro@ucrso.info>
2018-07-06 11:16:49 -0600

    Correcion 1 listar pedidos cocina.

--- PedidosExpress/BL/ManejadorOrden.cs
+++ PedidosExpress/BL/ManejadorOrden.cs
@@ -23,5 +23,6 @@ namespace BL
             }
             return lista;
         }
+
     }
 }
--- PedidosExpress/UI/CocinaListaPedidosWF.aspx.cs
+++ PedidosExpress/UI/CocinaListaPedidosWF.aspx.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BL;
 
 namespace UI
 {
     public partial class CocinaListaPedidosWF : System.Web.UI.Page
     {
+
+        ManejadorOrden bl = new ManejadorOrden();
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            bl.ListaOrdenes
         }
     }
 }
