0e2085674d20bb77fbe8752c0f562ff7adf1b771
Tanniesita <tannia.elizondo@ucrso.info>
2018-07-06 14:59:00 -0600

    Correccion del logueo de usuarios

--- PedidosExpress/UI/LoguearUsuarios.aspx.designer.cs
+++ PedidosExpress/UI/LoguearUsuarios.aspx.designer.cs
@@ -49,12 +49,12 @@ namespace UI {
         protected global::System.Web.UI.WebControls.TextBox txtPassword;
         
         /// <summary>
-        /// btnRegistrarAdmin control.
+        /// btnRegistrarUsuario control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnRegistrarAdmin;
+        protected global::System.Web.UI.WebControls.Button btnRegistrarUsuario;
     }
 }
