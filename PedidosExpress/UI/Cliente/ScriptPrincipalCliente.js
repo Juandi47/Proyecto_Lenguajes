5e4bbaa2ddf7f287c68c38d7a7d18dcdb6cfad0c
juandi perez <juandi199847@hotmail.com>
2018-07-11 19:00:53 -0600

    prueba

---
+++ PedidosExpress/UI/Cliente/ScriptPrincipalCliente.js
@@ -0,0 +1,40 @@
+function MostrarPlatos(){
+	var req = $.ajax({
+		url: "http://localhost:50408/WSRest/WSCliente.svc/listaPlatos",
+		timeout:10000,
+		dataType: "jsonp"
+	});
+
+	req.done(function(datos){
+		ProcesarPlatos(datos);
+	});
+
+	req.fail(function(){
+		alert("Error alconectar con el servicio");
+	});
+}
+
+function ProcesarPlatos(datos){
+
+	$.each(datos, function(){
+
+		var newLi = document.createElement("li");
+		var newdiv = document.createElement("div");
+		newdiv.setAttribute("id",this.nombrePlato);
+		newdiv.setAttribute("style","background-color:white");
+		var newRef = document.createElement("a");
+		newRef.innerHTML = "Ver plato";
+		newRef.setAttribute("id",this.nombrePlato);
+		newRef.setAttribute("href","Plato.html?codigoPlato=" + this.codigoPlato);
+		var newH = document.createElement("h4");
+		newH.innerHTML = "Plato: " + this.nombrePlato;
+		newdiv.appendChild(newH);
+		var newP = document.createElement("p");
+		newP.innerHTML += "Precio: " + this.precio;	
+		newdiv.appendChild(newP);
+		newdiv.appendChild(newRef);	
+		newLi.appendChild(newdiv);
+		$('#ListaPlatos').append(newLi);
+	});
+}
+
